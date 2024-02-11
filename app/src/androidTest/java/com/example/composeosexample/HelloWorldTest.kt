abc5e4270b0ca14a98bf3d43143c1fa8d2deb2d4
a-shunevych-engenious <stihia7@yahoo.com>
2024-02-11 15:17:10 +0200

    - update

--- app/src/androidTest/java/com/example/composeosexample/HelloWorldTest.kt
+++ app/src/androidTest/java/com/example/composeosexample/HelloWorldTest.kt
@@ -4,8 +4,12 @@ import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import org.junit.Test
+import java.util.*
+import kotlin.random.Random.Default.nextInt
 
 class HelloWorldTest : BaseTest() {
+
+    val random = nextLongValue(5000..30000L)
 
     @Test
     fun testByText(){
@@ -32,6 +36,108 @@ class HelloWorldTest : BaseTest() {
         activityTestRule
             .onNodeWithText("hello")
             .assertExists()
+    }
 
+    @Test
+    fun test_random_time(){
+        activityTestRule
+            .onNodeWithText("Click")
+            .performClick()
+
+        activityTestRule
+            .onNodeWithText("hello")
+            .assertExists()
+        Thread.sleep(random)
+    }
+
+
+    private fun nextLongValue(range: LongRange): Long {
+        return (range.first + nextInt((range.last - range.first + 1).toInt()))
+    }
+
+
+    @Test
+    fun test_3(){
+        activityTestRule
+            .onNodeWithText("Click")
+            .performClick()
+
+        activityTestRule
+            .onNodeWithText("hello")
+            .assertExists()
+    }
+
+    @Test
+    fun test_long_test_10_seconds_passed(){
+        Thread.sleep(random)
+        activityTestRule
+            .onNodeWithText("Click")
+            .performClick()
+
+        activityTestRule
+            .onNodeWithText("hello")
+            .assertExists()
+    }
+
+    @Test
+    fun test_long_3_test(){
+        Thread.sleep(random)
+        activityTestRule
+            .onNodeWithText("Click")
+            .performClick()
+
+        activityTestRule
+            .onNodeWithText("hello")
+            .assertExists()
+    }
+
+    @Test
+    fun test_long_1_test(){
+        Thread.sleep(random)
+        activityTestRule
+            .onNodeWithText("Click")
+            .performClick()
+
+        activityTestRule
+            .onNodeWithText("hello")
+            .assertExists()
+    }
+
+    @Test
+    fun test_long_2(){
+        Thread.sleep(random)
+        activityTestRule
+            .onNodeWithText("Click")
+            .assertDoesNotExist()
+
+
+        activityTestRule
+            .onNodeWithText("hello")
+            .assertExists()
+    }
+
+
+    @Test
+    fun test_long_test_20_seconds_passed(){
+        Thread.sleep(20_000)
+        activityTestRule
+            .onNodeWithText("Click")
+            .performClick()
+
+        activityTestRule
+            .onNodeWithText("hello")
+            .assertExists()
+    }
+
+    @Test
+    fun test_failed(){
+        activityTestRule
+            .onNodeWithText("Click")
+            .performClick()
+
+        activityTestRule
+            .onNodeWithText("hello")
+            .assertDoesNotExist()
+        Thread.sleep(20_000)
     }
 }
